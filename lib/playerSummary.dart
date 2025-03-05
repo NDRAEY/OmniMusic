ac064462f787fe3997dc16147e5da67bf2329147
Andrey <pikachu_andrey@vk.com>
2025-03-05 15:25:05 +0900

    Slider

--- lib/playerSummary.dart
+++ lib/playerSummary.dart
@@ -31,13 +31,37 @@ class _PlayerSummaryState extends State<PlayerSummary> {
   Widget build(BuildContext context) {
     var playButton = GestureDetector(
       onTap: () {
-        if(widget.state.player.state == PlayerState.playing) {
+        if (widget.state.player.state == PlayerState.playing) {
           widget.state.player.pause();
         } else {
           widget.state.player.resume();
         }
       },
-      child: Text(widget.state.player.state == PlayerState.playing ? "PAUSE" : "PLAY")
+      child: Text(
+        widget.state.player.state == PlayerState.playing ? "PAUSE" : "PLAY",
+      ),
+    );
+
+    var progress = Slider(
+      value: currentPosition?.inSeconds.toDouble() ?? 0.0,
+      max: widget.state.currentTrackInfo?.duration?.inSeconds.toDouble() ?? 0.0,
+      onChanged: (val) {},
+      padding: EdgeInsets.all(0.0),
+    );
+
+    var trackPosition = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          padding: EdgeInsets.only(right: 16.0),
+          child: Text(durationToTime(currentPosition)),
+        ),
+        Expanded(child: progress),
+        Container(
+          padding: EdgeInsets.only(left: 16.0),
+          child: Text(durationToTime(widget.state.currentTrackInfo?.duration)),
+        ),
+      ],
     );
 
     return GestureDetector(
@@ -51,20 +75,27 @@ class _PlayerSummaryState extends State<PlayerSummary> {
           borderRadius: BorderRadius.circular(4),
         ),
         padding: EdgeInsets.all(8),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            trackPosition,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(widget.state.currentTrackInfo?.title ?? "No Data", style: TextStyle(fontSize: 16)),
-                Text(widget.state.currentTrackInfo?.artist ?? "No Data"),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.state.currentTrackInfo?.title ?? "No Data",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Text(widget.state.currentTrackInfo?.artist ?? "No Data"),
+                  ],
+                ),
+                Text("BKWD"),
+                playButton,
+                Text("FWD"),
               ],
             ),
-            Text(durationToTime(currentPosition)),
-            Text("BKWD"),
-            playButton,
-            Text("FWD"),
           ],
         ),
       ),
